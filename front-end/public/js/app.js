a1de2a6991fd1d0e05e9a50e1fb8090b59388722
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-18 20:06:21 +0330

    Update app.js

--- front-end/public/js/app.js
+++ front-end/public/js/app.js
@@ -1,16 +1,18 @@
 const togglePasswordType = (input, icon) => {
-    const type = input.getAttribute("type") === "password" ? "text" : "password";
-    input.setAttribute("type", type);
-    icon.setAttribute("src", type === "text"
-        ? "public/icons/eye_invisible.png"
-        : "public/icons/eye_visible.png"
-    );
-}
+  const type = input.getAttribute("type") === "password" ? "text" : "password";
+  input.setAttribute("type", type);
+  icon.setAttribute(
+    "src",
+    type === "text"
+      ? "public/icons/eye_invisible.png"
+      : "public/icons/eye_visible.png"
+  );
+};
 
-document.querySelectorAll(".toggle-password").forEach(icon => {
-    icon.addEventListener("click", () => {
-        const inputId = icon.getAttribute("data-icon");
-        const input = document.getElementById(inputId);
-        togglePasswordType(input, icon);
-    });
+document.querySelectorAll(".toggle-password").forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const inputId = icon.getAttribute("data-icon");
+    const input = document.getElementById(inputId);
+    togglePasswordType(input, icon);
+  });
 });
