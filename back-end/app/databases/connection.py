69c24e92b7aa62c4437a34c41b742e69a8ba9bc1
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-23 12:36:41 +0330

    Update connection.py
    Edit databases info

--- back-end/app/databases/connection.py
+++ back-end/app/databases/connection.py
@@ -7,10 +7,10 @@ import os
 
 load_dotenv()
 
-DB_DOMAIN = f"{os.getenv('DB_DOMAIN').strip(' ')}+{os.getenv('DB_DRIVER').strip(' ')}"
-DB_USER = f"{os.getenv("DB_USERNAME").strip(' ')}:{os.getenv("DB_PASSWORD").strip(' ')}"
-DB_HOST = f"{os.getenv('DB_HOST').strip(' ')}:{os.getenv('DB_PORT').strip(' ')}"
-DB_NAME = os.getenv("DB_NAME").strip(' ')
+DB_DOMAIN = f"{os.getenv('DB_DOMAIN').strip()}+{os.getenv('DB_DRIVER').strip()}"
+DB_USER = f"{os.getenv("DB_USERNAME").strip()}:{os.getenv("DB_PASSWORD").strip()}"
+DB_HOST = f"{os.getenv('DB_HOST').strip()}:{os.getenv('DB_PORT').strip()}"
+DB_NAME = os.getenv("DB_NAME").strip()
 
 DB_URL = f"{DB_DOMAIN}://{DB_USER}@{DB_HOST}/{DB_NAME}"
 engine = create_engine(DB_URL)
