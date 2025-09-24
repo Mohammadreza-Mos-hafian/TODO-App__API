8e8fa09c08d0b0eeb122791135f8d46794e95b00
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-24 19:45:29 +0330

    Update user model

--- back-end/app/models/user.py
+++ back-end/app/models/user.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from typing import Optional
 
-from sqlalchemy import Boolean, DateTime, Identity, Integer, PrimaryKeyConstraint, String, UniqueConstraint, text, func
+from sqlalchemy import Boolean, DateTime, Identity, Integer, PrimaryKeyConstraint, String, UniqueConstraint, text, Uuid, \
+    func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from datetime import datetime
+from uuid import UUID
 
 from app.databases import Base
 
@@ -14,7 +16,8 @@ class User(Base):
     __tablename__ = 'users'
     __table_args__ = (
         PrimaryKeyConstraint('id', name='users_pkey'),
-        UniqueConstraint('email', name='users_email_key')
+        UniqueConstraint('email', name='users_email_key'),
+        UniqueConstraint('uuid', name='users_uuid_key')
     )
 
     id: Mapped[int] = mapped_column(Integer,
@@ -24,6 +27,7 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(String(32), nullable=False)
     email: Mapped[str] = mapped_column(String(64), nullable=False)
     password: Mapped[str] = mapped_column(String, nullable=False)
+    uuid: Mapped[UUID] = mapped_column(Uuid, nullable=False)
     is_deleted: Mapped[bool] = mapped_column(Boolean, nullable=False, server_default=text('false'))
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False,
                                                  server_default=text('CURRENT_TIMESTAMP'))
