136c48648129e583bb666fa6e1fc9e1c80881690
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-24 16:55:05 +0330

    Update pre load

--- back-end/app/schemas/auth/register_schema.py
+++ back-end/app/schemas/auth/register_schema.py
@@ -3,11 +3,8 @@ from typing import Dict
 from marshmallow import fields, validate, validates_schema, ValidationError, pre_load
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-from markupsafe import escape
-
 from app.models import User
-
-import re
+from app.utils import clean_data
 
 
 class RegisterSchema(SQLAlchemyAutoSchema):
@@ -31,12 +28,5 @@ class RegisterSchema(SQLAlchemyAutoSchema):
             raise ValidationError("Password confirmation does not match", field_name="confirmation_password")
 
     @pre_load
-    def clean_data(self, data: Dict[str, str], **kwargs) -> Dict[str, str]:
-        for key, value in data.items():
-            if isinstance(data[key], str):
-                data[key] = escape(re.sub(r"<.*?>", "", value))
-                data[key] = re.sub(r"\s+", " ", data[key]).strip()
-
-        data["email"] = re.sub(r"[^\w@.\-]", "", data["email"]).lower()
-
-        return data
+    def pre_load_data(self, data, **kwargs):
+        return clean_data(data)
