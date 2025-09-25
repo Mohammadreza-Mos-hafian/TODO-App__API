4a9f1197418f490f25ea0cbe0a34630dd4868bf5
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-26 02:25:21 +0330

    Create dashboard services

---
+++ back-end/app/services/dashboard_services.py
@@ -0,0 +1,21 @@
+from sqlalchemy.exc import SQLAlchemyError
+
+from flask_jwt_extended import get_jwt_identity
+
+from app.repositories import UserRepository
+
+
+class DashboardService:
+    @staticmethod
+    def get_user_name():
+        try:
+            data = UserRepository.get_user(get_jwt_identity())
+            return {
+                "status": "success",
+                "full_name": f"{data.first_name} {data.last_name}"
+            }
+        except SQLAlchemyError as err:
+            return {
+                "status": "DB Error",
+                "errors": str(err)
+            }
