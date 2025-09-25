e59b32025e49b3de3f59be65292e9a7931c282a3
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-26 02:29:37 +0330

    Update create app function

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
-from app.databases import ma
-from app.routes import auth_bp
+from app.routes import auth_bp, dash_bp
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
 
 
 def create_app():
@@ -10,8 +16,14 @@ def create_app():
 
     CORS(app)
 
-    ma.init_app(app)
-
     app.register_blueprint(auth_bp)
+    app.register_blueprint(dash_bp)
+
+    app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY").strip()
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_ACCESS_TOKEN_SECRET_KEY").strip()
+    app.config["JWT_REFRESH_SECRET_KEY"] = os.getenv("JWT_REFRESH_TOKEN_SECRET_KEY").strip()
+    app.config["DEBUG"] = os.getenv("DEBUG").strip() == "True"
+
+    JWTManager(app)
 
     return app
