6edaf155854209af772bfc9eb85fc95c3b028ad0
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-26 02:34:42 +0330

    Update auth services

--- back-end/app/services/auth_services.py
+++ back-end/app/services/auth_services.py
@@ -1,36 +1,73 @@
 from marshmallow import ValidationError
 
-from app.schemas import RegisterSchema
+from sqlalchemy.exc import SQLAlchemyError
+
+from app.schemas import RegisterSchema, LoginSchema
 from app.databases import SessionLocal
 from app.repositories import UserRepository
-from app.utils import encode, check_unique_email
+from app.utils import encode, check_unique_email, check_user
+from app.services.jwt_services import JWTService
 
 
 class AuthService:
     @staticmethod
-    def validate_user(data):
+    def register_user(data):
         schema = RegisterSchema()
 
         try:
             user = schema.load(data, session=SessionLocal)
-            unique_email = check_unique_email(user.email)
-
-            if not unique_email:
-                return {
-                    "status": "error",
-                    "errors": {
-                        "email": ["The email already exists."]
-                    }
-                }
-            elif isinstance(unique_email, dict):
-                return unique_email
+            check_unique_email(user.email)
 
             user.password = encode(user.password)
-            user_repo = UserRepository()
+            user_repo = UserRepository.create(user)
 
-            return user_repo.create(user)
+            return {
+                "status": "success",
+                "access_token": JWTService.create_access_token(str(user_repo.uuid)),
+                "refresh_token": JWTService.create_refresh_token(str(user_repo.uuid))
+            }
         except ValidationError as err:
             return {
                 "status": "error",
                 "errors": err.messages
+            }
+        except SQLAlchemyError as err:
+            return {
+                "status": "DB Error",
+                "errors": str(err)
+            }
+        except ValueError as err:
+            return {
+                "status": "Auth Error",
+                "errors": str(err)
+            }
+
+    @staticmethod
+    def login_user(data):
+        schema = LoginSchema()
+
+        try:
+            credentials = schema.load(data)
+            current_user = check_user(credentials["email"], credentials["password"])
+
+            return {
+                "status": "success",
+                "access_token": JWTService.create_access_token(str(current_user.uuid)),
+                "refresh_token": JWTService.create_refresh_token(str(current_user.uuid)),
+            }
+
+        except ValidationError as err:
+            return {
+                "status": "error",
+                "errors": err.messages
+            }
+        except SQLAlchemyError as err:
+            return {
+                "status": "DB Error",
+                "errors": str(err)
+            }
+        except ValueError as err:
+            return {
+                "status": "Auth Error",
+                "errors": str(err)
             }
