a665ab835717c604b76dcbfe7152ddfa53e9ae2f
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-28 02:56:33 +0330

    Update auth.js

--- front-end/public/js/auth/auth.js
+++ front-end/public/js/auth/auth.js
@@ -37,7 +37,7 @@ export const registerUser = async (
   }
 };
 
-export const loginUser = async (email, password, token) => {
+export const loginUser = async (email, password) => {
   try {
     const response = await fetch("http://127.0.0.1:5000/api/auth/login", {
       method: "POST",
@@ -64,39 +64,4 @@ export const loginUser = async (email, password, token) => {
       message: err,
     };
   }
-};
-
-/*--------------------------- Error Messages --------------------------- */
-
-export const showErrorMessages = (errors = {}) => {
-  for (let [key, value] of Object.entries(errors)) {
-    if (value[0] !== "") {
-      let inputParentElem = document.getElementById(`${key}`).parentElement;
-      let spanElem = inputParentElem.nextElementSibling;
-
-      if (spanElem && spanElem.classList.contains("error-message")) {
-        spanElem.textContent = value[0];
-        spanElem.classList.add("text-danger");
-      }
-    }
-  }
-};
-
-export const refreshErrors = () => {
-  document.querySelectorAll(".error-message").forEach((elem) => {
-    elem.textContent = "";
-    elem.classList.remove("text-danger");
-  });
-};
-
-/*--------------------------- Redirect if Authenticated --------------------------- */
-
-export const isAuthenticated = () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.getItem("access_token")) {
-      window.location.assign(
-        "http://127.0.0.1:3000/public/pages/dashboard.html"
-      );
-    }
-  });
 };
