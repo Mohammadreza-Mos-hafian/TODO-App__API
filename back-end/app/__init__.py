1bfc0bab506f37db3a30cca43a22fd903aeca08f
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-28 02:58:22 +0330

    register task routes

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 
-from app.routes import auth_bp, dash_bp
-
+from app.routes import auth_bp, dash_bp, task_bp
 from dotenv import load_dotenv
 
 import os
@@ -18,6 +17,7 @@ def create_app():
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(dash_bp)
+    app.register_blueprint(task_bp)
 
     app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY").strip()
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_ACCESS_TOKEN_SECRET_KEY").strip()
