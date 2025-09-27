025ceb7fcdeaa5f6a5491eb54b4b401d9f3eed22
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-28 02:56:43 +0330

    Update utils.js

--- front-end/public/js/utils.js
+++ front-end/public/js/utils.js
@@ -1,3 +1,5 @@
+/*--------------------------- Toggle Icon --------------------------- */
+
 const togglePasswordType = (input, icon) => {
   const type = input.getAttribute("type") === "password" ? "text" : "password";
   input.setAttribute("type", type);
@@ -9,10 +11,49 @@ const togglePasswordType = (input, icon) => {
   );
 };
 
-document.querySelectorAll(".toggle-password").forEach((icon) => {
-  icon.addEventListener("click", () => {
-    const inputType = icon.getAttribute("data-icon");
-    const input = document.getElementById(inputType);
-    togglePasswordType(input, icon);
+export const toggleEyeIcon = () => {
+  document.querySelectorAll(".toggle-password").forEach((icon) => {
+    icon.addEventListener("click", () => {
+      const inputType = icon.getAttribute("data-icon");
+      const input = document.getElementById(inputType);
+      togglePasswordType(input, icon);
+    });
   });
-});
+};
+
+/*--------------------------- Error Messages --------------------------- */
+
+export const showErrorMessages = (errors = {}) => {
+  for (let [key, value] of Object.entries(errors)) {
+    if (value[0] !== "") {
+      let inputParentElem = document.getElementById(`${key}`).parentElement;
+      let spanElem = inputParentElem.nextElementSibling;
+
+      if (spanElem && spanElem.classList.contains("error-message")) {
+        spanElem.textContent = value[0];
+        spanElem.classList.add("text-danger");
+      }
+    }
+  }
+};
+
+export const refreshErrors = () => {
+  document.querySelectorAll(".error-message").forEach((elem) => {
+    elem.textContent = "";
+    elem.classList.remove("text-danger");
+  });
+};
+
+/*--------------------------- Redirect if Authenticated --------------------------- */
+
+export const isAuthenticated = () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("access_token")) {
+      setTimeout(() => {
+        window.location.assign(
+          "http://127.0.0.1:3000/public/pages/dashboard.html"
+        );
+      }, 500);
+    }
+  });
+};
