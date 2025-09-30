07d34353913681e518efe42150d0ca5cf04009f4
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-01 02:36:33 +0330

    Update app.js

--- front-end/public/js/app.js
+++ front-end/public/js/app.js
@@ -6,3 +6,15 @@ const getYear = () => {
 };
 
 document.querySelector(".year").textContent = getYear();
+
+if (document.getElementById("logoutBtn")) {
+  document.getElementById("logoutBtn").addEventListener("click", () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
+    localStorage.removeItem("full_name");
+
+    setTimeout(() => {
+      window.location.assign("http://127.0.0.1:3000/public/index.html");
+    }, 500);
+  });
+}
