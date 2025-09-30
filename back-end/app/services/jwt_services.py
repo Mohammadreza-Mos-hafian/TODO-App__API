cabde8dce6938a23a704f4cc93cced438da1751b
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-30 20:30:49 +0330

    Update jwt_services.py

--- back-end/app/services/jwt_services.py
+++ back-end/app/services/jwt_services.py
@@ -1,32 +1,26 @@
-from flask import current_app
-
-from app.utils import now_utc
-
-from datetime import timedelta
-
-import jwt
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 
 class JWTService:
     @staticmethod
-    def create_access_token(subject: str, role: str = "user", expire_minutes: int = 15):
-        now = now_utc()
-        payload = {
-            "sub": subject,
-            "type": "access",
-            "role": role,
-            "iat": now,
-            "exp": now + timedelta(minutes=expire_minutes)
-        }
-        return jwt.encode(payload, current_app.config["JWT_SECRET_KEY"], algorithm="HS256")
+    def create_access_token(subject: str, role: str = "user"):
+        return create_access_token(
+            identity=subject,
+            additional_claims={
+                "type": "access",
+                "role": role,
+                "permissions": ["create", "read", "update", "delete"],
+                "is_verified": True
+            }
+        )
 
     @staticmethod
-    def create_refresh_token(subject: str, expire_days: int = 7):
-        now = now_utc()
-        payload = {
-            "sub": subject,
-            "type": "refresh",
-            "iat": now,
-            "exp": now + timedelta(days=expire_days)
-        }
-        return jwt.encode(payload, current_app.config["JWT_REFRESH_SECRET_KEY"], algorithm="HS256")
+    def create_refresh_token(subject: str, role: str = "user"):
+        return create_refresh_token(
+            identity=subject,
+            additional_claims={
+                "type": "refresh",
+                "role": role,
+                "is_active": True
+            }
+        )
