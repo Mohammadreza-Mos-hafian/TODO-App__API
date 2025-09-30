6bef8a6eac68df01043c0682aa861de6723f2c21
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-30 20:31:15 +0330

    Create task routes

--- back-end/app/routes/task_routes.py
+++ back-end/app/routes/task_routes.py
@@ -7,4 +7,4 @@ task_bp = Blueprint("tasks", __name__, url_prefix="/api/tasks")
 task_view = TaskView.as_view("task_view")
 
 task_bp.add_url_rule("/", view_func=task_view, methods=["POST"])
-task_bp.add_url_rule("/<string:uuid>", view_func=task_view, methods=["GET", "PUT", "DELETE"])
+task_bp.add_url_rule("/<string:user_uuid>", view_func=task_view, methods=["GET", "PUT", "DELETE"])
