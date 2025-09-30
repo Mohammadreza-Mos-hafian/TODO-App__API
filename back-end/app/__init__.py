07770405f73fee30d853ba96bc0f160de7b9d392
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-09-30 20:28:06 +0330

    Update CORS

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -5,6 +5,8 @@ from flask_jwt_extended import JWTManager
 from app.routes import auth_bp, dash_bp, task_bp
 from dotenv import load_dotenv
 
+from datetime import timedelta
+
 import os
 
 load_dotenv()
@@ -13,7 +15,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
-    CORS(app)
+    CORS(app, supports_credentials=True, origins=["http://127.0.0.1:3000"])
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(dash_bp)
@@ -22,6 +24,9 @@ def create_app():
     app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY").strip()
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_ACCESS_TOKEN_SECRET_KEY").strip()
     app.config["JWT_REFRESH_SECRET_KEY"] = os.getenv("JWT_REFRESH_TOKEN_SECRET_KEY").strip()
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=5)
+    app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+
     app.config["DEBUG"] = os.getenv("DEBUG").strip() == "True"
 
     JWTManager(app)
