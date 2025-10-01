57b076f563ffc3bfd914311c0a3a67603f551107
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-02 02:12:26 +0330

    Edit task routes

--- back-end/app/routes/task_routes.py
+++ back-end/app/routes/task_routes.py
@@ -1,10 +1,14 @@
 from flask import Blueprint
 
-from app.views import TaskView
+from app.views import UserTaskView, AdminTaskView
 
-task_bp = Blueprint("tasks", __name__, url_prefix="/api/tasks")
+task_bp = Blueprint("tasks", __name__, url_prefix="/api")
 
-task_view = TaskView.as_view("task_view")
+user_task_view = UserTaskView.as_view("user_task_view")
+admin_task_view = AdminTaskView.as_view("admin_task_view")
 
-task_bp.add_url_rule("/", view_func=task_view, methods=["POST"])
-task_bp.add_url_rule("/<string:user_uuid>", view_func=task_view, methods=["GET", "PUT", "DELETE"])
+task_bp.add_url_rule("/tasks", view_func=user_task_view, methods=["POST", "GET"])
+task_bp.add_url_rule("/tasks/<task_uuid>", view_func=user_task_view, methods=["PUT", "DELETE"])
+
+task_bp.add_url_rule("/users/<user_uuid>/tasks", view_func=admin_task_view, methods=["GET"])
+task_bp.add_url_rule("/users/<user_uuid>/tasks/<task_uuid>", view_func=admin_task_view, methods=["PUT", "DELETE"])
