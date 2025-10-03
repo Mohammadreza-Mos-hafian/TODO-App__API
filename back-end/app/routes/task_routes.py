ab398580aa8c1a32e6e214894e7068052b526abf
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-04 01:59:42 +0330

    Update task_routes.py

--- back-end/app/routes/task_routes.py
+++ back-end/app/routes/task_routes.py
@@ -8,7 +8,7 @@ user_task_view = UserTaskView.as_view("user_task_view")
 admin_task_view = AdminTaskView.as_view("admin_task_view")
 
 task_bp.add_url_rule("/tasks", view_func=user_task_view, methods=["POST", "GET"])
-task_bp.add_url_rule("/tasks/<task_uuid>", view_func=user_task_view, methods=["PUT", "DELETE"])
+task_bp.add_url_rule("/tasks/<task_uuid>", view_func=user_task_view, methods=["GET", "PATCH", "DELETE"])
 
 task_bp.add_url_rule("/users/<user_uuid>/tasks", view_func=admin_task_view, methods=["GET"])
 task_bp.add_url_rule("/users/<user_uuid>/tasks/<task_uuid>", view_func=admin_task_view, methods=["PUT", "DELETE"])
