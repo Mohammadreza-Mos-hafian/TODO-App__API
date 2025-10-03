e37e84afa68adaba1df68bc62afeb2c6a0035c2d
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-04 01:58:37 +0330

    validate deadline

--- back-end/app/schemas/task/create_schema.py
+++ back-end/app/schemas/task/create_schema.py
@@ -1,11 +1,13 @@
-from marshmallow import validate, pre_load, fields
+from marshmallow import validate, pre_load, fields, validates, ValidationError
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from flask_jwt_extended import get_jwt_identity
 
 from app.models import Task
-from app.utils import clean_data, create_uuid4
+from app.utils import clean_data, create_uuid4, now_datatime
 from app.repositories import UserRepository
+
+from datetime import date
 
 
 class CreateSchema(SQLAlchemyAutoSchema):
@@ -19,6 +21,14 @@ class CreateSchema(SQLAlchemyAutoSchema):
                        validate=validate.Length(min=1, max=32, error="The title must be between 1 and 32 characters."))
     deadline = fields.Date(required=True, allow_none=False)
     description = fields.Str(validate=validate.Length(max=500))
+
+    @validates("deadline")
+    def validate_deadline(self, value: str, **kwargs):
+        user_deadline = str(value).strip().split("-")
+        deadline = date(int(user_deadline[0]), int(user_deadline[1]), int(user_deadline[2]))
+
+        if deadline < date.today():
+            raise ValidationError(f"The deadline must be after {now_datatime().strftime('%Y-%m-%d')}.")
 
     @pre_load
     def pre_load_data(self, data, **kwargs):
