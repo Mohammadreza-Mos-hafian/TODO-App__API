636f03683215df454ff055fdcc53285a11e72e31
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-04 02:00:33 +0330

    Update task_schema.py

--- back-end/app/schemas/task/task_schema.py
+++ back-end/app/schemas/task/task_schema.py
@@ -2,7 +2,7 @@ from marshmallow import post_dump, pre_dump
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from app.models import Task
-from app.utils import show_task_status, task_status_color
+from app.utils import task_status_color
 
 
 class TaskSchema(SQLAlchemyAutoSchema):
@@ -14,6 +14,5 @@ class TaskSchema(SQLAlchemyAutoSchema):
     @post_dump
     def post_dump_data(self, task, **kwargs):
         task["color"] = task_status_color(task["status"])
-        task["status"] = show_task_status(task["status"])
 
         return task
