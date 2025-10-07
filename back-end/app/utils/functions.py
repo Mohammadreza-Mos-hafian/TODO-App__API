13efd3ecdb3dc9899d9fc96213c0b9913a0ec189
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-07 21:20:08 +0330

    Update functions.py

--- back-end/app/utils/functions.py
+++ back-end/app/utils/functions.py
@@ -1,15 +1,6 @@
 from typing import Dict
 
-from sqlalchemy import select, and_
-from sqlalchemy.exc import SQLAlchemyError
-
-from app.enums import TaskStatus
-from app.databases import SessionLocal
-from app.models import User, Task
-
-from markupsafe import escape
-
-from datetime import datetime, date
+from datetime import datetime
 
 import bcrypt, re, uuid
 
@@ -25,91 +16,26 @@ def now_datatime():
     return datetime.strptime(now, time_format)
 
 
-def create_uuid4() -> str:
-    return str(uuid.uuid4())
-
-
-def check_unique_email(email: str):
-    with SessionLocal() as session:
-        try:
-            stmt = (
-                select(User).where(
-                    User.email == email,
-                    User.is_deleted == False
-                )
-            )
-
-            if session.execute(stmt).scalar():
-                raise ValueError("The email already exists.")
-
-        except SQLAlchemyError as err:
-            session.rollback()
-            raise err
-
-
-def check_user(email: str, password: str):
-    with SessionLocal() as session:
-        try:
-            stmt = (
-                select(User)
-                .where(and_(
-                    User.email == email,
-                    User.is_deleted == False
-                ))
-            )
-
-            user: User = session.execute(stmt).scalars().first()
-
-            if not user or not bcrypt.checkpw(password.encode(), user.password.encode()):
-                raise ValueError("Email or password is incorrect.")
-
-            return user
-        except SQLAlchemyError as err:
-            session.rollback()
-            raise err
+def create_uuid4() -> uuid.UUID:
+    return uuid.uuid4()
 
 
 def clean_data(data: Dict[str, str]) -> Dict[str, str]:
     for key, value in data.items():
         if isinstance(data[key], str):
-            data[key] = escape(re.sub(r"<.*?>", "", value))
+            data[key] = re.sub(r"<.*?>", "", value)
             data[key] = re.sub(r"\s+", " ", data[key]).strip()
 
     return data
 
 
-def task_status_color(number):
-    colors = ["warning", "info", "success", "danger"]
+def pagination_info(page: int, per_page: int, total: int):
+    total_pages = (total + per_page - 1) // per_page
 
-    index = next((status.value for status in TaskStatus if status.value == number))
-
-    return colors[index]
-
-
-def validate_date(task_uuid, deadline):
-    clean_date = str(deadline).strip().split("-")
-    user_deadline = date(int(clean_date[0]), int(clean_date[1]), int(clean_date[2]))
-
-    with SessionLocal() as session:
-        try:
-            stmt = (
-                select(Task)
-                .where(
-                    Task.uuid == task_uuid,
-                    Task.is_deleted == False
-                )
-            )
-
-            task = session.execute(stmt).scalars().first()
-
-            clean_task_deadline = str(task.deadline).strip().split("-")
-            task_deadline = date(int(clean_task_deadline[0]), int(clean_task_deadline[1]), int(clean_task_deadline[2]))
-
-            if user_deadline == task_deadline:
-                return
-
-            if user_deadline < date.today():
-                raise ValueError(f"The deadline must be after {now_datatime().strftime('%Y-%m-%d')}.")
-        except SQLAlchemyError as err:
-            session.rollback()
-            raise err
+    return {
+        "status": "success",
+        "page": page,
+        "per_page": per_page,
+        "total": total,
+        "total_pages": total_pages,
+    }
