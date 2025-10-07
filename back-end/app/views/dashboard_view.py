dfa155fb015dffb409cf7354c2f36f90f704af65
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-07 21:18:27 +0330

    Update dashboard_view.py

--- back-end/app/views/dashboard_view.py
+++ back-end/app/views/dashboard_view.py
@@ -2,11 +2,20 @@ from flask import jsonify, request
 from flask.views import MethodView
 from flask_jwt_extended import jwt_required
 
+from sqlalchemy.exc import SQLAlchemyError
+
 from app.services import DashboardService
 
 
 class DashboardView(MethodView):
     @jwt_required()
     def get(self):
-        response = DashboardService.get_user_name()
-        return jsonify(response)
+        try:
+            response = DashboardService.get_user_name()
+            return jsonify(response), 200
+
+        except SQLAlchemyError as err:
+            return jsonify({
+                "status": "DB Error",
+                "errors": str(err)
+            }), 500
