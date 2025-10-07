9dd7abf1f302dde0bcbc5a718de8f0875dd6500b
Mohammadreza <82016386+GG-Mohammadreza-GG@users.noreply.github.com>
2025-10-07 21:25:56 +0330

    Update task_routes.py

--- back-end/app/routes/task_routes.py
+++ back-end/app/routes/task_routes.py
@@ -1,14 +1,10 @@
 from flask import Blueprint
 
-from app.views import UserTaskView, AdminTaskView
+from app.views import TaskView
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/api")
 
-user_task_view = UserTaskView.as_view("user_task_view")
-admin_task_view = AdminTaskView.as_view("admin_task_view")
+user_task_view = TaskView.as_view("user_task_view")
 
 task_bp.add_url_rule("/tasks", view_func=user_task_view, methods=["POST", "GET"])
 task_bp.add_url_rule("/tasks/<task_uuid>", view_func=user_task_view, methods=["GET", "PATCH", "DELETE"])
-
-task_bp.add_url_rule("/users/<user_uuid>/tasks", view_func=admin_task_view, methods=["GET"])
-task_bp.add_url_rule("/users/<user_uuid>/tasks/<task_uuid>", view_func=admin_task_view, methods=["PUT", "DELETE"])
